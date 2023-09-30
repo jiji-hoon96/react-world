4e10e5f5ab7e90aadf2d786a91de0bfd948d9811
JIHOON LEE <jihoon7705@gmail.com>
2023-09-30 14:42:00 +0900

    feat : taglist 생성

---
+++ src/components/TagList/index.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import * as styles from './index.css';
+
+export default function TagList() {
+  const { data } = useQuery(['tagList'], () =>
+    fetch('https://api.realworld.io/api/tags').then((res) => res.json())
+  );
+  console.log(data);
+  return (
+    <div className={styles.TagContainer}>
+      <p className={styles.TagTitle}>Popular Tags</p>
+      {data?.tags.map((x: string) => (
+        <li key={} className={styles.TagItem}>
+          {x}
+        </li>
+      ))}
+    </div>
+  );
+}
